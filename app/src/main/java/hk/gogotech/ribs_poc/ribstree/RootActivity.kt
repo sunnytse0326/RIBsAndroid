2e6ff789d7ca5f75c2f284d4d2a0ee82cfde7da5
Sunny Tse <sunny.tse@gogotech.hk>
2018-10-30 11:06:01 +0800

    first user second user env setup

--- app/src/main/java/hk/gogotech/ribs_poc/ribstree/RootActivity.kt
+++ app/src/main/java/hk/gogotech/ribs_poc/ribstree/RootActivity.kt
@@ -1,16 +1,16 @@
 package hk.gogotech.ribs_poc.ribstree
 
+import android.os.Build
 import android.view.ViewGroup
 import com.uber.rib.core.RibActivity
 import com.uber.rib.core.ViewRouter
+import hk.gogotech.ribs_poc.BuildConfig
 import hk.gogotech.ribs_poc.model.OrderItem
 import hk.gogotech.ribs_poc.model.SendBirdUser
 import hk.gogotech.ribs_poc.ribstree.root.RootBuilder
 import hk.gogotech.ribs_poc.storage.LocalStorage
 
 class RootActivity : RibActivity() {
-    val userID1 = "12345345342564"
-    val userID2 = "985737575325"
 
     override fun createRouter(parentViewGroup: ViewGroup): ViewRouter<*, *, *> {
         val rootBuilder = RootBuilder(object : RootBuilder.ParentComponent {
@@ -22,10 +22,9 @@ class RootActivity : RibActivity() {
     }
 
     private fun getLocalStorage(): LocalStorage {
-        // TODO user data are required to handle separately
-        //val user = SendBirdUser(userID2, "SunnyTse22222", "ORDERID199999", "https://media.whatscap.com/4f0/66a/4f066a068ebbd865fdf66af8288f894aca8e5a84_n.png")
-        val user = SendBirdUser(userID1, "SunnyTse", "ORDERID19999", "https://media.whatscap.com/204/442/20444223eba1e5ce6d989287ccfcf45f9c08f045_b.jpg")
-        val order = OrderItem(userID1, userID2)
+        val myID = BuildConfig.ID
+        val user = SendBirdUser(myID.toString(), BuildConfig.userName, "ORDERID199999", "https://media.whatscap.com/4f0/66a/4f066a068ebbd865fdf66af8288f894aca8e5a84_n.png")
+        val order = OrderItem(BuildConfig.firstUserID.toString(), BuildConfig.secondUserID.toString())
         return LocalStorage(user, order)
     }
 }
